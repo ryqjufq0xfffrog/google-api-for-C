5af250b2731528d6c26e838f5a028a8a6f8cb494
ryqjufq0xfffrog <ryqjufqvrnyhuxewucnpubvj@gmail.com>
2025-09-15 08:28:20 +0900

    http headers

--- sample.c
+++ sample.c
@@ -7,15 +7,15 @@
 #include "./src/http_utils.h"
 
 int main(){
-  curl_slist* headers;
+  struct curl_slist* headers = NULL;
   unsigned short respCode = 0;
 
-  headers = curl_slist_append
+  headers = curl_slist_append(headers, "Content-Type: text/plain");
 
   BinData* ptr = http_req(
     "https://echo.free.beeceptor.com",
     "POST",
-    NULL, NULL, 0, &respCode
+    headers, NULL, 0, &respCode
   );
 
   printf("%lu bytes retrieved\n", (unsigned long) ptr ->size);
--- src/http_utils.h
+++ src/http_utils.h
@@ -1,1 +1,1 @@
-BinData* http_req(char* url, char* method, struct curl_slist* headers, size_t (*read_callback)(char*, size_t, size_t, void*), size_t bodySize);
+BinData* http_req(char* url, char* method, struct curl_slist* headers, size_t (*read_callback)(char*, size_t, size_t, void*), size_t bodySize, unsigned short* code);
